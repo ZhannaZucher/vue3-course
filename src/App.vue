0cf39c2839e4fceca70efa21b64330a5df438639
ZhannaZucher <jzucher@gmail.com>
2024-05-09 17:25:49 +0200

    add: binding inputs  values to reactive data

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,80 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+	<div class="app">
+		<form>
+			<h4>Création d'un post</h4>
+			<input v-bind:value="title" @input="inputTitle" class="input" type="text" placeholder="Nom">
+			<input v-bind:value="body" @input="inputBody" class="input" type="text" placeholder="Description">
+			<button class="btn" @click="createPost">Créer un post</button>
+			<p>Le titre sera : {{ title }}</p>
+		</form>
+		<div class="post" v-for="post in posts" :key="post.id">
+			<div><strong>Nom: </strong>{{ post.title }}</div>
+			<div><strong>Description: </strong>{{ post.body }}</div>
+		</div>
+	</div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			posts: [
+				{ id: 1, title: 'Javascript 1', body: 'Je suis un post sur JS' },
+				{ id: 2, title: 'Javascript 2', body: 'Je suis un post sur JS' },
+				{ id: 3, title: 'Javascript 3', body: 'Je suis un post sur JS' },
+			],
+			title: '',
+			body: ''
+		}
+	},
+	methods: {
+		createPost() {
+
+		},
+		inputTitle(event) {
+			this.title = event.target.value
+		},
+		inputBody(event) {
+			this.body = event.target.value
+		}
+	}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+	padding: 0;
+	margin: 15px 0 0 0;
+	box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+	padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+form {
+	display: flex;
+	flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.input {
+	width: 100%;
+	border: 1px solid teal;
+	padding: 10px 15px;
+	margin-top: 15px;
+}
+
+.btn {
+	margin-top: 15px;
+	align-self: flex-end;
+	padding: 10px 15px;
+	background: none;
+	color: teal;
+	border: 1px solid teal;
+}
+
+.post {
+	padding: 15px;
+	border: 2px solid teal;
 }
 </style>
