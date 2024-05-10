880ef9feeba56279adf78b8d5fde0d0b81cbfa6c
ZhannaZucher <jzucher@gmail.com>
2024-05-10 17:33:10 +0200

    add: conditional list rendering

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,11 @@
 <template>
 
-	<div>
+	<div v-if="posts.length > 0">
 		<h3 class="posts-header">Les posts</h3>
+		<post-item v-for="post in posts" :key="post.id" :post="post" @remove="$emit('remove', post)" />
 	</div>
-	<post-item v-for="post in posts" :key="post.id" :post="post" @remove="$emit('remove', post)" />
+	<h3 v-else>Il n'y a pas de posts</h3>
+
 </template>
 
 <script>
