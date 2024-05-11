3294fef2a870483020f224e992bea91ac3c22041
ZhannaZucher <jzucher@gmail.com>
2024-05-11 18:08:31 +0200

    create: custom select

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,29 @@
+<template>
+	<select :value="modelValue" @change="changeOption">
+		<option disabled value="">Choisir une option</option>
+		<option v-for="option in options" :key="option.value" :value="option.value">{{ option.name }}</option>
+	</select>
+</template>
+
+<script>
+export default {
+	name: 'my-select',
+	emits: ['update:model-value'],
+	props: {
+		modelValue: {
+			type: String
+		},
+		options: {
+			type: Array,
+			default: () => []
+		},
+	},
+	methods: {
+		changeOption(event) {
+			this.$emit('update:modelValue', event.target.value)
+		}
+	}
+}
+</script>
+
+<style scoped></style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,8 @@
 import MyButton from '@/components/UI/MyButton'
 import MyInput from '@/components/UI/MyInput'
 import MyModal from '@/components/UI/MyModal'
+import MySelect from '@/components/UI/MySelect'
 
 export default [
-	MyButton, MyInput, MyModal
+	MyButton, MyInput, MyModal, MySelect
 ]
