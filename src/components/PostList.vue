fbe031e18ce8e9d46eb61f9d3d536bc86006d807
ZhannaZucher <jzucher@gmail.com>
2024-05-19 12:14:04 +0200

    add: transition-group for post list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -2,7 +2,9 @@
 
 	<div v-if="posts.length > 0">
 		<h3 class="posts-header">Les posts</h3>
-		<PostItem v-for="post in posts" :key="post.id" :post="post" @remove="$emit('remove', post)" />
+		<TransitionGroup name="list">
+			<PostItem v-for="post in posts" :key="post.id" :post="post" @remove="$emit('remove', post)" />
+		</TransitionGroup>
 	</div>
 	<h3 v-else>Il n'y a pas de posts</h3>
 
@@ -27,5 +29,27 @@ export default {
 <style scoped>
 .posts-header {
 	margin-bottom: 15px;
+}
+
+/*styles for transition-group*/
+.list-item {
+	display: inline-block;
+	margin-right: 10px;
+}
+
+.list-enter-active,
+.list-leave-active {
+	transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+	opacity: 0;
+	transform: translateX(130px);
+}
+
+/** -move animates reordering posts list on sort select */
+.list-move {
+	transition: transform 0.8s ease;
 }
 </style>
